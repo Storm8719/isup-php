a7f871c1debdcc02d55880816f24e7389de4e709
oleg <storm8719@gmail.com>
2022-12-05 00:31:16 +0300

    refactor main js

--- web/js/main.js
+++ web/js/main.js
@@ -1,63 +1,63 @@
-// fetch('/main/get-websites-list').then((response) => console.log(response));
+const websitesInfoPath = '/check/'
 
+const liveSearchLogic = () => {
+    const searchInput = document.getElementById('liveSearch');
+    const resultBox = document.getElementById('resultBox');
 
-// fetch('/main/get-websites-list')
-//     .then((response)=>{
-//         console.log(response)
-//     })
-const searchInput = document.getElementById('liveSearch');
-const resultBox = document.getElementById('resultBox');
-
-const getAllWebsites = async () => {
-    let response = await fetch('/main/get-websites-list');
-    if (response.ok) {
-        return await response.json();
-    } else {
-        console.log("Error HTTP: " + response.status);
+    const getAllWebsites = async () => {
+        let response = await fetch('/main/get-websites-list');
+        if (response.ok) {
+            return await response.json();
+        } else {
+            console.log("Error HTTP: " + response.status);
+        }
     }
-}
 
-const initLiveSearch = async () => {
-    searchInput.removeEventListener('focus', initLiveSearch);
-    const websites = await getAllWebsites();
-    const fuse = new Fuse(websites, {
-        keys: ['url']
-    });
-    renderLiveSearchResults(fuse.search(searchInput.value));
-    ['keyup'].forEach((eventType) => {
-        searchInput.addEventListener(eventType, () => {
-            renderLiveSearchResults(fuse.search(searchInput.value));
+    const initLiveSearch = async () => {
+        searchInput.removeEventListener('focus', initLiveSearch);
+        const websites = await getAllWebsites();
+        const fuse = new Fuse(websites, {
+            keys: ['url']
         });
-    })
-    searchInput.addEventListener('focus', () => {
         renderLiveSearchResults(fuse.search(searchInput.value));
-    })
-    searchInput.addEventListener('focusout', () => {
-        setTimeout(()=> {
-            renderLiveSearchResults(null);
-        }, 100)
-    })
-}
+        ['keyup'].forEach((eventType) => {
+            searchInput.addEventListener(eventType, () => {
+                renderLiveSearchResults(fuse.search(searchInput.value));
+            });
+        })
+        searchInput.addEventListener('focus', () => {
+            renderLiveSearchResults(fuse.search(searchInput.value));
+        })
 
-searchInput.addEventListener('focus', initLiveSearch)
+        searchInput.addEventListener('focusout', () => {
+            setTimeout(() => {
+                renderLiveSearchResults(null);
+            }, 100)
+        })
 
-
-const renderLiveSearchResults = (results) => {
-
-    if(!results){
-        resultBox.innerHTML = '';
-        return false;
     }
 
-    const fragment = document.createDocumentFragment();
+    searchInput.addEventListener('focus', initLiveSearch)
 
-    results.forEach((item) => {
-        const listItem = document.createElement('a');
-        listItem.innerText = item.item.url;
-        listItem.href = '#';
-        fragment.appendChild(listItem);
-    })
+    const renderLiveSearchResults = (results) => {
 
-    resultBox.innerHTML = '';
-    resultBox.appendChild(fragment);
+        if (!results) {
+            resultBox.innerHTML = '';
+            return false;
+        }
+
+        const fragment = document.createDocumentFragment();
+
+        results.forEach((item) => {
+            const listItem = document.createElement('a');
+            listItem.innerText = item.item.url;
+            listItem.href = `${websitesInfoPath}${item.item.url}`;
+            fragment.appendChild(listItem);
+        })
+
+        resultBox.innerHTML = '';
+        resultBox.appendChild(fragment);
+    }
 }
+
+liveSearchLogic();
