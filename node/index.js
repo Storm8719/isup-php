7acb312e7879703f83458a4d041483253c2b1e15
oleg <storm8719@gmail.com>
2023-01-14 19:49:43 +0300

    make worker class for checking

--- node/index.js
+++ node/index.js
@@ -1,65 +1,95 @@
 const puppeteer = require('puppeteer');
+const Queue = require('./queue');
 
-const makeScreenshot = async (url, imgName, browser) => {
 
-    const page = await browser.newPage();
-    await page.setViewport({
-        width: 1920,
-        height: 1080,
-        deviceScaleFactor: 1,
-    })
+class worker
+{
+    initialized = false;
+    pagesOpened = 0;
+    pagesLimit = 3;
+    browser;
+    queue;
 
-    // await page.goto(url, {waitUntil: 'networkidle0', timeout: 0});
-    // console.time('FirstWay'+imgName);
-    const response = await page.goto(url, {timeout: 0});
-    // const navigationPromise = page.waitForNavigation({ waitUntil: ['networkidle2'] });
-    const responseInfo = {
-        timing:response.timing(),
-        status: response.status(),
-        headers: response.headers()
+
+    constructor() {
+        this.queue = new Queue();
+        puppeteer.launch({ headless: false }).then((browser) => {
+            this.browser = browser;
+            this.initialized = true;
+            setInterval(()=>{
+                this.resolveQueue();
+            },1)
+        });
     }
 
-    const js_result_from_page = await page.evaluate(() => {
-        const meta_desc = document.querySelector('meta[name="description"]');
-        const description = (meta_desc !== null && meta_desc.content) ? meta_desc.content : null;
+    resolveQueue = () => {
+        if (this.pagesOpened < this.pagesLimit && this.queue.length) {
+            this.execute(this.queue.dequeue());
+        }
+    }
 
-        return {
-            title:document.title,
-            description: description,
-        };
-    });
+    makeScreenshot = (url, imgName) => {
+        return new Promise((resolve, reject) => {
+            const resultCall = (res) => {
+                resolve(res)
+            }
+            this.queue.enqueue({url, imgName, callback:resultCall});
+        });
+    }
 
-    await page.screenshot({
-        path: './' + imgName + '.png',
-        type: 'png',
-        fullPage: true
-    })
 
-    await page.close();
-    return {...js_result_from_page, ...responseInfo};
+    execute = async ({url, imgName, callback}) => {
+        this.pagesOpened++;
+        const page = await this.browser.newPage();
+
+        await page.setViewport({
+            width: 1920,
+            height: 1080,
+            deviceScaleFactor: 1,
+        })
+        await page.setDefaultTimeout(60000);
+
+        const response = await page.goto(url, {waitUntil:"load"});
+
+        const responseInfo = {
+            timing:response.timing(),
+            status: response.status(),
+            headers: response.headers()
+        }
+
+        const js_result_from_page = await page.evaluate(() => {
+            const meta_desc = document.querySelector('meta[name="description"]');
+            const description = (meta_desc !== null && meta_desc.content) ? meta_desc.content : null;
+
+            return {
+                title:document.title,
+                description: description,
+            };
+        });
+
+        await page.screenshot({
+            path: './' + imgName + '.jpeg',
+            type: 'jpeg',
+            quality: 50,
+        })
+        await page.close();
+        this.pagesOpened--;
+
+        this.resolveQueue();
+        console.log(`DONE ${imgName}`);
+
+        callback({...js_result_from_page, ...responseInfo});
+        return {...js_result_from_page, ...responseInfo};
+    }
 }
 
 (async () => {
-    const browser = await puppeteer.launch({ headless: false });
-    const actions = [];
+    const checker = new worker();
 
-    const listUrls = [
-        'https://example.com',
-        'https://www.php.net'
-    ];
-
-    let i = 0;
-    listUrls.forEach((el) => {
-        i++;
-        actions.push(makeScreenshot(el, 'page-' + i, browser).then((result) => {
-            // console.log(result);
-            return result;
-        }));
-    })
-
-    const result = await Promise.all(actions);
-    console.log(result);
-
-
-    await browser.close();
+    for (let i = 16; i !== 0; i--) {
+        checker.makeScreenshot('http://isup/main/timeout?time='+i, 'page-'+i).then((res)=>{
+            // console.log(res);
+        })
+        // console.log(i)
+    }
 })();
