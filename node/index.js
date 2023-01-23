78443461aa38d82ba5cf88cb87d163a3a9d0dc87
oleg <storm8719@gmail.com>
2023-01-23 20:48:30 +0300

    connect screnshots microservice to rabbitmq transport

--- node/index.js
+++ node/index.js
@@ -1,36 +1,18 @@
 const worker = require('./worker');
-const MicroMQ = require('micromq');
+const rabbitmqq = require('./rabbitmqq');
 
 
-const app = new MicroMQ({
-    name: 'node-checker-with-screenshots',
-    rabbit: {
-        url: 'amqp://guest:guest@localhost:5672',
-    },
-});
-
+const rabbitTransportService = new rabbitmqq('amqp://guest:guest@localhost:5672');
 
 const checker = new worker();
 
-app.get('/check-website', (req, res) => {
+const checkAndScreenHandler = ({id, url, imgName = null}) => {
 
-    checker.makeScreenshot(1 ,'http://isup/main/timeout?time='+1, 'page-'+1).then((res)=>{
-        console.log(res);
+    imgName = imgName ? imgName : `image-${id}`;
+
+    checker.makeScreenshot(id ,url, imgName).then((res)=>{
+        rabbitTransportService.send('check-and-screen-results', res);
     })
+}
 
-    res.json({status:"ok"});
-
-
-});
-
-
-(() => {
-    const checker = new worker();
-
-    for (let i = 2; i !== 0; i--) {
-        checker.makeScreenshot(i ,'http://isup/main/timeout?time='+i, 'page-'+i).then((res)=>{
-            console.log(res);
-        })
-        // console.log(i)
-    }
-})();
+rabbitTransportService.subscribeOnMessages('check-and-screen', checkAndScreenHandler);
