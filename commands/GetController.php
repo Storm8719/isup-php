d637e025c554771490f2aa06d16f947b3249a54d
oleg <storm8719@gmail.com>
2023-01-23 20:21:13 +0300

    Add test commands for rabbitmqq

--- commands/GetController.php
+++ commands/GetController.php
@@ -3,29 +3,18 @@
 
 namespace app\commands;
 
-use PhpAmqpLib\Connection\AMQPStreamConnection;
-use PhpAmqpLib\Message\AMQPMessage;
+use app\daemon\RabbitMQQ;
 
 
 class GetController extends \yii\console\Controller
 {
 
-    public function actionIndex($message = 'hello world')
+    public function actionIndex()
     {
-        $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
-        $channel = $connection->channel();
-        $queueName = 'test';
-        $channel->queue_declare($queueName, false, false, false, false);
-
-        $callback = function ($msg) {
-            echo ' [x] Received ', $msg->body, "\n";
-        };
-
-        $channel->basic_consume($queueName, '', false, true, false, false, $callback);
-
-        while ($channel->is_open()) {
-            $channel->wait();
-        }
+        $q = new RabbitMQQ('localhost', 5672, 'guest', 'guest');
+        $q->subscribeOnMessages('test', function($msg){
+            echo $msg.PHP_EOL;
+        });
     }
 
 }
--- commands/SendController.php
+++ commands/SendController.php
@@ -4,27 +4,18 @@
 namespace app\commands;
 
 
-use app\daemon\WebsiteCheckerService;
-use app\models\Sites;
-use PhpAmqpLib\Connection\AMQPStreamConnection;
-use PhpAmqpLib\Message\AMQPMessage;
-use Yii;
-use yii\console\ExitCode;
+use app\daemon\RabbitMQQ;
 
 class SendController extends \yii\console\Controller
 {
 
-    public function actionIndex($message = 'hello world')
+    public function actionIndex()
     {
-        $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
-        $channel = $connection->channel();
-        $queueName = 'test';
-        $channel->queue_declare($queueName, false, false, false, false);
-        $msg = new AMQPMessage(json_encode('Hello World!'));
-        $channel->basic_publish($msg, '', $queueName);
-        echo " [x] Sent 'Hello World!'\n";
-        $channel->close();
-        $connection->close();
+        $q = new RabbitMQQ('localhost', 5672, 'guest', 'guest');
+        $q->send('test', 'Hello JS');
+        $q->send('test', 'Hello PHP');
+        $q->send('test', 'Hello JS PHP');
+        $q->send('test', 'Hello JS PHP');
     }
 
 }
