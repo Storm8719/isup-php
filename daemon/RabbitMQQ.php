327f2703a13e666d58cacd920a8c61891b3026ac
oleg <storm8719@gmail.com>
2023-01-26 20:14:01 +0300

    refactor rmqq php

--- daemon/RabbitMQQ.php
+++ daemon/RabbitMQQ.php
@@ -11,31 +11,54 @@ class RabbitMQQ
 {
     private $connection;
     private $channel;
+//    private $messagesToSendQueue;
+    private $connectionCreds;
 
     public function __construct($host, $port, $user, $password)
     {
-        $this->init($host, $port, $user, $password);
+//        $this->messagesToSendQueue = new \SplQueue();
+
+        $this->connectionCreds = [
+            'host' => $host,
+            'port' => $port,
+            'user' => $user,
+            'password' => $password
+            ];
+
+        $this->init();
     }
 
-    protected function init($host, $port, $user, $password){
+    protected function init(){
+
         try {
-            $this->connection = new AMQPStreamConnection($host, $port, $user, $password);
+            $this->connection = new AMQPStreamConnection($this->connectionCreds['host'], $this->connectionCreds['port'], $this->connectionCreds['user'], $this->connectionCreds['password']);
             $this->channel = $this->connection->channel();
         } catch (\Exception $e) {
             \Yii::$app->l->log("Connection to rabbit failed. Try to reconnect in 1s...");
             sleep(1);
-            $this->init($host, $port, $user, $password);
+            $this->init();
         }
     }
 
     public function send($toQueueName, $message){
-        $this->channel->queue_declare($toQueueName, false, false, false, false);
+        try {
+            $this->channel->queue_declare($toQueueName, false, false, false, false);
+        }catch (\Exception $e){
+//            $this->messagesToSendQueue->enqueue(["toQueueName" => $toQueueName, "message" => $message]);
+            $this->init();
+        }
+
         $msg = new AMQPMessage(json_encode($message));
         $this->channel->basic_publish($msg, '', $toQueueName);
+
     }
 
     public function subscribeOnMessages($messagesToListenQueueName, $callback){
-        $this->channel->queue_declare($messagesToListenQueueName, false, false, false, false);
+        try {
+            $this->channel->queue_declare($messagesToListenQueueName, false, false, false, false);
+        }catch (\Exception $e){
+            $this->init();
+        }
 
         $callbackDecode = function ($msg) use ($callback){
             $callback(json_decode($msg->body));
