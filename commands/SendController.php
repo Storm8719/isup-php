1ea9fd9c14e32d72cde38ae56e7b25dd981b9792
oleg <storm8719@gmail.com>
2023-02-03 21:58:49 +0300

    try to send event to moleculer

--- commands/SendController.php
+++ commands/SendController.php
@@ -5,18 +5,46 @@ namespace app\commands;
 
 
 use app\daemon\RabbitMQQ;
+use stdClass;
 
 class SendController extends \yii\console\Controller
 {
 
     public function actionIndex()
     {
-        $q = new RabbitMQQ('localhost', 5672, 'guest', 'guest');
-        $q->send('check-and-screen', ['id' => 5,'url' => 'http://isup/', 'imgName'=> 'www'] );
 
-        $q->subscribeOnMessages('check-and-screen-results', function($msg){
-            var_dump($msg);
-        });
+        $obj = new stdClass();
+//        $obj->id = "47a0facd-1187-4945-b546-be9281281396";
+        $obj->event = "screenshots.make";
+        $obj->data = new stdClass();
+        $obj->data->user = "Oleg S";
+        $obj->groups = ["screenshots"];
+//        $obj->broadcast = false;
+//        $obj->meta = new stdClass();
+//        $obj->level = 1;
+//        $obj->tracing = null;
+//        $obj->requestID = "5f0d4f05-a9fd-4fa8-855c-a9ee8d88ad00";
+//        $obj->caller = null;
+//        $obj->needAck = null;
+        $obj->ver = "4";
+//        $obj->sender = "desktop-pv134n5-1808";
+
+
+        $q = new RabbitMQQ('localhost', 5672, 'guest', 'guest');
+        $q->send('MOL.EVENT.desktop-pv134n5-6356', $obj);
+
+
+//        var_dump($obj);
+
+//        while(true){
+//            $q->send('check-and-screen-2', ['id' => 5,'url' => 'http://isup/', 'imgName'=> 'www'] );
+//            sleep(3);
+//            echo 'Sended ...'.PHP_EOL;
+//        }
+
+//        $q->subscribeOnMessages('check-and-screen-results', function($msg){
+//            var_dump($msg);
+//        });
 //        $q->send('test', 'Hello PHP');
 //        $q->send('test', 'Hello JS PHP');
 //        $q->send('test', 'Hello JS PHP');
